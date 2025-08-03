8af3bf8e382e8ab8cc7c7b1f50a77c5fc5fbf9cc
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-03 16:11:29 +0300

    Kafka, Schema Registry and Redis configuration (#2)
    * Refactors deployment to use service names.
    
    * 60s -> 90s
    
    * Refactored workflows and updated Avro version.

--- solo-leveling-notification-service/src/main/kotlin/com/sleepkqq/sololeveling/notification/config/KafkaConfig.kt
+++ solo-leveling-notification-service/src/main/kotlin/com/sleepkqq/sololeveling/notification/config/KafkaConfig.kt
@@ -13,12 +13,12 @@ import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 
+@Suppress("unused")
 @EnableKafka
 @Configuration
-@Suppress("unused")
 class KafkaConfig(
-	@Value("\${spring.kafka.bootstrap-servers}") bootstrapServers: String,
-	@Value("\${spring.kafka.properties.schema.registry.url}") schemaRegistryUrl: String
+	@Value($$"${spring.kafka.bootstrap-servers}") bootstrapServers: String,
+	@Value($$"${spring.kafka.properties.schema.registry.url}") schemaRegistryUrl: String
 ) : DefaultKafkaConfig(bootstrapServers, schemaRegistryUrl) {
 
 	@Bean
